82edf937a8e972eac521e440a9a349e7e5fba146
Suguru Hirayama <super206cc@gmail.com>
2024-06-27 21:00:41 +0900

    Update App.js with VotingForm component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,90 @@
 import React from 'react';
 import './App.css';
+import VotingForm from './VotingForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>医療コピーライティングコンテスト</h1>
+      </header>
+      <main>
+        <VotingForm />
+      </main>
+    </div>
+  );
+}
+
+export default App;import React from 'react';
+import './App.css';
+import VotingForm from './VotingForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>医療コピーライティングコンテスト</h1>
+      </header>
+      <main>
+        <VotingForm />
+      </main>
+    </div>
+  );
+}
+
+export default App;import React from 'react';
+import './App.css';
+import VotingForm from './VotingForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>医療コピーライティングコンテスト</h1>
+      </header>
+      <main>
+        <VotingForm />
+      </main>
+    </div>
+  );
+}
+
+export default import React from 'react';
+import './App.css';
+import VotingForm from './VotingForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>医療コピーライティングコンテスト</h1>
+      </header>
+      <main>
+        <VotingForm />
+      </main>
+    </div>
+  );
+}
+
+export default Apimport React from 'react';
+import './App.css';
+import VotingForm from './VotingForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>医療コピーライティングコンテスト</h1>
+      </header>
+      <main>
+        <VotingForm />
+      </main>
+    </div>
+  );
+}
+
+export default App;import React from 'react';
+import './App.css';
 import MyComponent from './MyComponent';
 import AnotherComponent from './AnotherComponent';
 
